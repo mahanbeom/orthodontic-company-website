72df58d025d18d1457b8e7cd671ee3cbfc04c515
mahanbeom <akgksqja@gmail.com>
2023-01-15 13:16:51 +0900

    feat: Add angular material module

--- orthodontic/src/app/app.module.ts
+++ orthodontic/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalNavBarComponent } from './global-nav-bar/global-nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ orthodontic/src/app/shared.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+// material modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+
+
+const materialModules = [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    MatTableModule,
+];
+
+
+const modules = [
+    HttpClientModule,
+    ...materialModules
+];
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        ...modules
+    ],
+    exports: [
+        ...modules
+    ],
+    providers: [
+
+    ]
+})
+export class SharedModule { }
